41b83fc345807895534f8ef2246c0cbccdd20e51
Christopher DeVito <christopher.devito@protonmail.com>
2024-11-17 18:14:21 -0500

    update expo root layout

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,10 +1,29 @@
-import { Provider } from 'app/provider'
-import { Stack } from 'expo-router'
+import { Provider } from 'app/provider';
+import { Stack } from 'expo-router';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { View } from 'dripsy';
+import { useEffect } from 'react';
+import * as ScreenOrientation from 'expo-screen-orientation';
+
+const MainLayout = () => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View
+      style={{ flex: 1, paddingTop: insets.top, paddingBottom: insets.bottom }}
+    >
+      <Stack screenOptions={{ headerShown: false }} />
+    </View>
+  );
+};
 
 export default function Root() {
+  useEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+  });
   return (
     <Provider>
-      <Stack />
+      <MainLayout />
     </Provider>
-  )
+  );
 }
